757462d95e5b528806c254d0a8a6db060bbf85d0
inovagenesissolucoes <inovagenesis.solucoes@gmail.com>
2026-06-17 09:51:27 -0300

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,6 @@
+// ============================================================
+// js/config.js — URL do Apps Script
+// ============================================================
+const CONFIG = {
+  API_URL: 'https://script.google.com/macros/s/AKfycbzDFGfG6fC2j4alSs4eG__j8CZ5EIS97IyzbSejRfOoYvfEIxbvAyPehDMz3rcVFU4J/exec'
+};
